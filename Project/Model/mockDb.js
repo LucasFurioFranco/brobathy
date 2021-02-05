f50800357577e4d8147253cdf47a3c1c8f1cee03
Lucas Fúrio Franco <lucasfuriofranco@gmail.com>
2021-02-04 22:42:36 -0300

    stricture

---
+++ Project/Model/mockDb.js
@@ -0,0 +1,16 @@
+window.controller = new (function() {
+  var internalDB = {
+    products: {
+      name: "name",
+      priceFrom: 99.90,
+      proceTo: 89.90,
+      stock: 100
+    }
+  };
+
+  return {
+    getProducts: function() {
+
+    }
+  }
+})()
